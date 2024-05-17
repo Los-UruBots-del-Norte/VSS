641719e1e5195e023b1fa72ea153bc3433143488
Vdeberserk <145728264+Vdeberserk@users.noreply.github.com>
2024-05-17 18:30:37 -0300

    Update receptor_motor.ino

--- receptor_motor.ino
+++ receptor_motor.ino
@@ -5,22 +5,55 @@ RF24 radio(7, 8); // CE, CSN
 
 const byte identificacion[6] = "00001";
 
-void setup() 
-{
-  Serial.begin(9600);
-  radio.begin();
-  radio.openWritingPipe(identificacion);
-  radio.setPALevel(RF24_PA_MIN);
-  radio.stopListening();
+// Pins para controlar el puente H
+const int IN1 = 2;
+const int IN2 = 3;
+const int IN3 = 4;
+const int IN4 = 5;
 
-  Serial.println("Conexion con el emisor exitosa");
+void setup() {
+  Serial.begin(9600);
+  
+  radio.begin();
+  radio.openReadingPipe(0, identificacion);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
+
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+  detenerMotores();
+
+  Serial.println("Receptor listo");
 }
 
-void loop() 
-{
- if (Serial.available() > 0) {
-    char command = Serial.read();  // Leer el comando enviado desde la PC
-    Serial.println(command);  // Imprimir el comando en el monitor serial
-    radio.write(&command, sizeof(command));
+void loop() {
+  if (radio.available()) {
+    char command;
+    radio.read(&command, sizeof(command));
+    
+    if (command == 'X') {
+      avanzar();
+    } else if (command == 'S') {
+      detenerMotores();
+    }
   }
+}
+
+void avanzar() {
+  Serial.println("Encendiendo motores hacia adelante");
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+void detenerMotores() {
+  Serial.println("Deteniendo motores");
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
 }
